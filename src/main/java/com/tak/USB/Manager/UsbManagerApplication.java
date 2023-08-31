1b46816e913efcc720f4a77201b48e876763b7c6
Inchan Tak <ict970@gmail.com>
2023-08-31 21:53:37 +0900

    USB manager

---
+++ src/main/java/com/tak/USB/Manager/UsbManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tak.USB.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsbManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsbManagerApplication.class, args);
+	}
+
+}
